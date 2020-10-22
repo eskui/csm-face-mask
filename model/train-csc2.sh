a440d41ec47a070617c229c32cb827180a5011a2
Aki Rehn <aki.rehn@helsinki.fi>
2020-10-22 12:10:33 +0300

    Add new versions of trainer code.
    Uses sigmoid output and predicts a single value probability of a mask or
    not.
    
    Also does random resizing of the images to avoid learning the size of
    the images.
    
    Added a new notebook that utilized the new code.

---
+++ model/train-csc2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=Project_2003564
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=0:20:00
+#SBATCH --gres=gpu:v100:1
+
+module load pytorch/1.6
+srun python3 train2.py
+
